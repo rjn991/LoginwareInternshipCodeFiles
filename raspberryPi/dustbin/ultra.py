1275989666cc655977ca99a47cdc1229c8fe1535
rjn991 <ranjanht991@gmail.com>
2023-08-24 13:22:54 +0530

    done for the day

---
+++ raspberryPi/dustbin/ultra.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+trig=16
+echo=18
+R=29
+G=31
+B=33
+GPIO.setup(trig,GPIO.OUT)
+GPIO.setup(echo,GPIO.IN)
+GPIO.setup(R,GPIO.OUT)
+GPIO.setup(G,GPIO.OUT)
+GPIO.setup(B,GPIO.OUT)
+def ultra_data():
+	GPIO.output(trig,1)
+	time.sleep(0.05)
+	GPIO.output(trig,0)
+
+	while(GPIO.input(echo)==0):
+		start_time = time.time()
+	while(GPIO.input(echo)==1):
+		stop_time = time.time()
+
+	duration = stop_time - start_time
+	d = (duration *34300)/2
+	if(d>10):
+		GPIO.output(R,1)
+		GPIO.output(G,0)
+		GPIO.output(B,1)
+	elif(d<10 and d>5):
+		GPIO.output(R,1)
+		GPIO.output(G,1)
+		GPIO.output(B,0)
+	elif(d<5):
+		GPIO.output(R,0)
+		GPIO.output(G,1)
+		GPIO.output(B,1)
+	#print(round(d,2),"cm")
+	return d
